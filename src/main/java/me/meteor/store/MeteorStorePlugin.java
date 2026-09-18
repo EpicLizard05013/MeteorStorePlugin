5b08a813e3cce28880ae9fc7d299a88a24d125d9
EpicLizard05013 <bailinwu610@gmail.com>
2026-09-18 16:30:14 -0400

    Refactor code redemption and reward logic

--- src/main/java/me/meteor/store/MeteorStorePlugin.java
+++ src/main/java/me/meteor/store/MeteorStorePlugin.java
@@ -10,20 +10,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public class MeteorStorePlugin extends JavaPlugin implements CommandExecutor {
 
-    // Store active codes: Code -> PurchaseInfo
+    // Store active codes: Code -> PurchaseData
     private final Map<String, PurchaseData> pendingPurchases = new HashMap<>();
 
     @Override
     public void onEnable() {
-        this.getCommand("redeem").setExecutor(this);
+        if (this.getCommand("redeem") != null) {
+            this.getCommand("redeem").setExecutor(this);
+        }
         getLogger().info("MeteorStorePlugin enabled successfully!");
     }
 
-    // Method to register a newly generated web code dynamically
+    // Register web codes dynamically
     public void registerCode(String code, String rewardType, double price) {
         pendingPurchases.put(code.toUpperCase(), new PurchaseData(rewardType, price));
     }
@@ -50,27 +51,70 @@ public class MeteorStorePlugin extends JavaPlugin implements CommandExecutor {
         }
 
         PurchaseData data = pendingPurchases.get(inputCode);
-
-        // Consume code immediately to prevent spam/duplication
         pendingPurchases.remove(inputCode);
 
-        // Execute reward logic
         grantReward(player, data.rewardType);
-        player.sendMessage(ChatColor.GREEN + "Code redeemed! Run /pay server " + (int)data.price + "m to finish payment.");
+        player.sendMessage(ChatColor.GREEN + "Code redeemed! Run /pay server " + (int) data.price + "m to finish payment.");
 
         return true;
     }
 
     private void grantReward(Player player, String rewardType) {
+        String name = player.getName();
         switch (rewardType.toLowerCase()) {
+            // Ranks
+            case "meteor":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + name + " parent set meteor");
+                break;
+            case "meteorplus":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + name + " parent set meteor+");
+                break;
+            case "meteorplusplus":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + name + " parent set meteor++");
+                break;
             case "inferno":
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " parent set inferno");
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + name + " parent set inferno");
                 break;
+            case "infernoplus":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + name + " parent set inferno+");
+                break;
+            case "infernoplusplus":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + name + " parent set inferno++");
+                break;
+
+            // Keys
+            case "common":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "crate give " + name + " common 15");
+                break;
+            case "gold":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "crate give " + name + " gold 35");
+                break;
+            case "prime":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "crate give " + name + " prime 5");
+                break;
+
+            // Shards
             case "shards100":
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "shards give " + player.getName() + " 100");
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "shards give " + name + " 100");
                 break;
+            case "shards400":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "shards give " + name + " 400");
+                break;
+            case "shards1500":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "shards give " + name + " 1500");
+                break;
+            case "shards3000":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "shards give " + name + " 3000");
+                break;
+            case "shards8000":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "shards give " + name + " 8000");
+                break;
+            case "shards10000":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "shards give " + name + " 10000");
+                break;
+
             default:
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "say Granted " + rewardType + " to " + player.getName());
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "say Granted " + rewardType + " to " + name);
                 break;
         }
     }
